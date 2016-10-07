1c573028617f04b1bc75516aec086daaf85f5be0
Pierre-Saigot <psaigot@gmail.com>
2016-10-07 11:14:21 +0200

    V0.1

---
+++ app/colorclass.js
@@ -0,0 +1,11 @@
+class Tool{
+	/**
+	 * Represente une couleur de la palette
+	 * @param  {string} name  nom de la couleur
+	 * @param  {array} color  les couleurs
+	 */
+	constructor(name , color){
+				this.name 		= name;
+				this.color		= color;
+			}
+}
---
+++ app/toolclass.js
@@ -0,0 +1,11 @@
+class Tool{
+	/**
+	 * [constructor description]
+	 * @param  {string} name      [description]
+	 * @param  {string} icon_file [description]
+	 */
+	constructor(name, icon_file){
+				this.name 		= name;
+				this.icon_file	= icon;
+			}
+}
